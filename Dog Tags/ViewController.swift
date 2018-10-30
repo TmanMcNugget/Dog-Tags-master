c5fd1504d86ceb35738876155e3ed68f619432e0
jminor131 <jason.km@aol.com>
2018-10-30 09:20:22 -0500

    Initial Commit

---
+++ Dog Tags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dog Tags
+//
+//  Created by period2 on 10/29/18.
+//  Copyright © 2018 period2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
